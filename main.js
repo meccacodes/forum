d560da0533ee4b029ff3587dbf08866809fae6cf
meccacodes <hi@windfallyarn.com>
2024-11-13 14:17:29 -0800

    Posts appear in posts div when submitted.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// your code here
+const post = document.getElementById("submit");
+const user = document.getElementById("name");
+const message = document.getElementById("message");
+const postBox = document.querySelector(".posts");
+
+const listenForPost = post.addEventListener("click", function () {
+  const userName = user.value;
+  const userMessage = message.value;
+  const userPostContainer = document.createElement("div");
+  const userPost = `<h4>${userName}</h4><p>${userMessage}<p>`;
+  userPostContainer.innerHTML = userPost;
+  postBox.insertAdjacentElement("afterbegin", userPostContainer);
+});
